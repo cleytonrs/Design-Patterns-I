1fa1e48e034b9ef893dd5a0ddf18b8c174d73a11
Cleyton Rodrigues <cleytonrs09@gmail.com>
2020-07-16 20:30:54 -0300

    Design Patterns - Strategy

---
+++ DesignPatterns/DesignPatterns/ISS.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns
+{
+    public class ISS : ITax
+    {
+        public double Calculate(Budget budget)
+        {
+            return budget.Value * 0.06;
+        }
+    }
+}
